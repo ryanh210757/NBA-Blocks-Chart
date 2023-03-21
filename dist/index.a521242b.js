3536dc649627c07be0f3998c93c0e4b9f1a16567
ryanh210757 <kryanharris1@gmail.com>
2023-03-21 16:57:43 -0500

    player data

--- dist/index.a521242b.js
+++ dist/index.a521242b.js
@@ -564,44 +564,206 @@ var _api = require("./api");
 (async function() {
     const data = [
         {
-            year: 2003,
-            count: 20.9
+            year: 1,
+            count: 220
         },
         {
-            year: 2004,
-            count: 27.2
+            year: 2,
+            count: 448
         },
         {
-            year: 2005,
-            count: 31.4
+            year: 3,
+            count: 700
         },
         {
-            year: 2006,
-            count: 27.3
+            year: 4,
+            count: 914
         },
         {
-            year: 2007,
-            count: 30
+            year: 5,
+            count: 1196
         },
         {
-            year: 2008,
-            count: 28.4
+            year: 6,
+            count: 1793
         },
         {
-            year: 2009,
-            count: 29.7
+            year: 7,
+            count: 2097
+        },
+        {
+            year: 8,
+            count: 2439
+        },
+        {
+            year: 9,
+            count: 2736
+        },
+        {
+            year: 10,
+            count: 2978
+        },
+        {
+            year: 11,
+            count: 3185
+        },
+        {
+            year: 12,
+            count: 3358
+        },
+        {
+            year: 13,
+            count: 3454
+        },
+        {
+            year: 14,
+            count: 3577
+        },
+        {
+            year: 15,
+            count: 3647
+        },
+        {
+            year: 16,
+            count: 3735
+        },
+        {
+            year: 17,
+            count: 3825
+        }
+    ];
+    const ad = [
+        {
+            year: 1,
+            count: 112
+        },
+        {
+            year: 2,
+            count: 301
+        },
+        {
+            year: 3,
+            count: 501
+        },
+        {
+            year: 4,
+            count: 626
+        },
+        {
+            year: 5,
+            count: 793
+        },
+        {
+            year: 6,
+            count: 986
+        },
+        {
+            year: 7,
+            count: 1121
+        },
+        {
+            year: 8,
+            count: 1264
+        },
+        {
+            year: 9,
+            count: 1323
+        },
+        {
+            year: 10,
+            count: 1413
+        },
+        {
+            year: 11,
+            count: 1509
+        }
+    ];
+    const rudy = [
+        {
+            year: 1,
+            count: 41
+        },
+        {
+            year: 2,
+            count: 230
+        },
+        {
+            year: 3,
+            count: 365
+        },
+        {
+            year: 4,
+            count: 494
+        },
+        {
+            year: 5,
+            count: 708
+        },
+        {
+            year: 6,
+            count: 837
+        },
+        {
+            year: 7,
+            count: 1024
+        },
+        {
+            year: 8,
+            count: 1159
+        },
+        {
+            year: 9,
+            count: 1349
+        },
+        {
+            year: 10,
+            count: 1486
+        }
+    ];
+    const myles = [
+        {
+            year: 1,
+            count: 86
+        },
+        {
+            year: 2,
+            count: 258
+        },
+        {
+            year: 3,
+            count: 376
+        },
+        {
+            year: 4,
+            count: 575
+        },
+        {
+            year: 5,
+            count: 707
+        },
+        {
+            year: 6,
+            count: 866
+        },
+        {
+            year: 7,
+            count: 984
+        },
+        {
+            year: 8,
+            count: 1122
         }
     ];
     new (0, _autoDefault.default)(document.getElementById("acquisitions"), {
-        type: "bar",
+        type: "line",
         options: {
             animation: true,
             plugins: {
                 legend: {
-                    display: false
+                    display: true
                 },
                 tooltip: {
-                    enabled: false
+                    enabled: true
                 }
             }
         },
@@ -609,15 +771,27 @@ var _api = require("./api");
             labels: data.map((row)=>row.year),
             datasets: [
                 {
-                    label: "Acquisitions by year",
+                    label: "Hakeem Olajuwon ",
                     data: data.map((row)=>row.count)
+                },
+                {
+                    label: "Anthony Davis",
+                    data: ad.map((row)=>row.count)
+                },
+                {
+                    label: "Rudy Gobert",
+                    data: rudy.map((row)=>row.count)
+                },
+                {
+                    label: "Myles Turner",
+                    data: myles.map((row)=>row.count)
                 }
             ]
         }
     });
 })();
 
-},{"chart.js/auto":"d8NN9","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./api":"8Zgej"}],"d8NN9":[function(require,module,exports) {
+},{"chart.js/auto":"d8NN9","./api":"8Zgej","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"d8NN9":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _chartJs = require("../dist/chart.js");
